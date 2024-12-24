66d01aefc4e35d13025166a8ad1f3f3207b0834c
unknown <bitstarwalkin@foxmail.com>
2024-12-24 16:02:46 +0800

    updates

--- src/components/sections/ComparisonBar.vue
+++ src/components/sections/ComparisonBar.vue
@@ -2,22 +2,30 @@
 
 <template>
   <div class="comparison-bar">
-    <el-row :gutter="20">
-      <el-col :span="8" v-for="(comparison, index) in comparisons" :key="index">
-        <div class="comparison-column">
-          <!-- 参考图像 -->
-          <div class="reference-image">
-            <el-image :src="comparison.referenceImage" fit="contain" />
-          </div>
-          <!-- 姿势GIF -->
-          <div class="pose-gif">
-            <el-image :src="comparison.poseGif" fit="contain" />
-          </div>
-          <!-- 对比视频GIF -->
-          <div class="comparative-videos">
-            <el-image :src="comparison.comparativeVideo1" fit="contain" />
-            <el-image :src="comparison.comparativeVideo2" fit="contain" />
-          </div>
+    <!-- 对于每一个比较案例，创建一行 -->
+    <el-row :gutter="20" v-for="(comparison, index) in comparisons" :key="index" class="comparison-row">
+      <!-- 参考图像 -->
+      <el-col :span="4">
+        <div class="reference-image">
+          <el-image :src="comparison.referenceImage" fit="contain" />
+        </div>
+      </el-col>
+      <!-- 姿势GIF -->
+      <el-col :span="4">
+        <div class="pose-gif">
+          <el-image :src="comparison.poseGif" fit="contain" />
+        </div>
+      </el-col>
+      <!-- ControlNext GIF -->
+      <el-col :span="8">
+        <div class="comparative-video">
+          <el-image :src="comparison.comparativeVideo1" fit="contain" />
+        </div>
+      </el-col>
+      <!-- EnergyVid GIF -->
+      <el-col :span="8">
+        <div class="comparative-video">
+          <el-image :src="comparison.comparativeVideo2" fit="contain" />
         </div>
       </el-col>
     </el-row>
@@ -39,14 +47,14 @@ export default {
         {
           referenceImage: "./comparison/case2/reference.png",
           poseGif: "./comparison/case2/pose.gif",
-          comparativeVideo1: "./comparison/case1/controlnext.gif",
-          comparativeVideo2: "./comparison/case1/energyvid.gif",
+          comparativeVideo1: "./comparison/case2/controlnext.gif",
+          comparativeVideo2: "./comparison/case2/energyvid.gif",
         },
         {
           referenceImage: "./comparison/case3/reference.jpg",
           poseGif: "./comparison/case3/pose.gif",
-          comparativeVideo1: "./comparison/case1/controlnext.gif",
-          comparativeVideo2: "./comparison/case1/energyvid.gif",
+          comparativeVideo1: "./comparison/case3/controlnext.gif",
+          comparativeVideo2: "./comparison/case3/energyvid.gif",
         },
       ]
     }
@@ -58,21 +66,21 @@ export default {
 .comparison-bar {
   padding: 20px;
 }
-.comparison-column {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+
+.comparison-row {
+  margin-bottom: 40px;
 }
+
 .reference-image,
 .pose-gif,
-.comparative-videos {
-  margin-bottom: 15px;
+.comparative-video {
+  text-align: center;
 }
-.comparative-videos {
-  display: flex;
-  justify-content: center;
-}
-.comparative-videos .el-image {
-  margin: 0 5px;
+
+.reference-image .el-image,
+.pose-gif .el-image,
+.comparative-video .el-image {
+  max-width: 100%;
+  height: auto;
 }
 </style>
